eb6e61efd2c9c49289e11aad6ac7c2c110d13ca1
fabym93 <fgalvn@byupathway.edu>
2026-01-10 17:56:29 -0300

    exercise 1-3 in C#

--- week01/Exercise3/Program.cs
+++ week01/Exercise3/Program.cs
@@ -4,6 +4,57 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise3 Project.");
+        Random randomGenerator = new Random();
+        bool playAgain = true;
+
+        while (playAgain)
+        {
+            int magicNumber = randomGenerator.Next(1, 101); // 1 to 100
+            int guess = -1;
+            int attempts = 0;
+
+            Console.WriteLine("Guess the number between 1 and 100.");
+
+            while (guess != magicNumber)
+            {
+                Console.Write("What is your guess? ");
+                string input = Console.ReadLine();
+                if (!int.TryParse(input, out guess))
+                {
+                    Console.WriteLine("Please enter a valid number.");
+                    continue;
+                }
+
+                attempts++;
+
+                if (magicNumber > guess)
+                {
+                    Console.WriteLine("Higher");
+                }
+                else if (magicNumber < guess)
+                {
+                    Console.WriteLine("Lower");
+                }
+                else
+                {
+                    Console.WriteLine("You guessed it!");
+                }
+            }
+
+            // Inform how many guesses were made
+            Console.WriteLine($"It took you {attempts} guesses to guess the number.");
+
+            // Ask if the user wants to play again
+            Console.Write("Would you like to play again? (yes/no): ");
+            string response = Console.ReadLine().Trim().ToLower();
+
+            if (response != "yes")
+            {
+                playAgain = false;
+            }
+            // If response is "yes", the loop continues and a new game starts
+        }
+
+        Console.WriteLine("Thanks for playing. Bye!");
     }
 }
