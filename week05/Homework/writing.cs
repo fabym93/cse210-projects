3d9d120f6298ac6af19be5716efa4112330aea8d
fabym93 <fgalvn@byupathway.edu>
2026-02-06 16:33:54 -0300

    homework

---
+++ week05/Homework/writing.cs
@@ -0,0 +1,20 @@
+public class WritingAssignment : Assignment
+{
+    private string _title;
+
+    
+    public WritingAssignment(string studentName, string topic, string title)
+        : base(studentName, topic)
+    {
+        // variables specific to the WritingAssignment class
+        _title = title;
+    }
+
+    public string GetWritingInformation()
+    {
+        // 
+        string studentName = GetStudentName();
+
+        return $"{_title} by {studentName}";
+    }
+}
